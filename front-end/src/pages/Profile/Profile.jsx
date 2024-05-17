35673e4ba81cd6af7370e297ae64994a0fac87f6
SeelyksDev <kevincordier.dev@gmail.com>
2024-05-17 19:30:44 +0200

    Transformation edit username into component

--- front-end/src/pages/Profile/Profile.jsx
+++ front-end/src/pages/Profile/Profile.jsx
@@ -1,11 +1,9 @@
 import accountData from "../../data/accountData.json";
 import Account from "../../components/Account/Account";
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import EditUsername from "../../components/EditUsername/EditUsername";
 import "./Profile.css";
-import UserIcon from "../../assets/user-icon.png";
-import { updateUserNameAPI } from "../../api/updateUsernameAPI";
-import { getUserInfo } from "../../store/userSlice";
 
 const Profile = () => {
     useEffect(() => {
@@ -14,84 +12,13 @@ const Profile = () => {
     }, []);
 
     const [isOpened, setIsOpened] = useState(false);
-    const [usernameValue, setUsernameValue] = useState("");
-    const [formError, setFormError] = useState(null);
-    const user = useSelector((state) => state.user);
-    const dispatch = useDispatch();
 
-    const updateUsername = async (e) => {
-        e.preventDefault();
-        const { error } = await updateUserNameAPI(user.token, usernameValue);
-        if (error) {
-            setFormError(error);
-        } else {
-            setIsOpened(false);
-            dispatch(getUserInfo(user.token));
-        }
-    };
+    const user = useSelector((state) => state.user);
 
     return (
         <main className="main bg-dark">
             {isOpened ? (
-                <section className="update-username-content">
-                    <img
-                        src={UserIcon}
-                        className="main-user_logo"
-                        alt="user logo"
-                    />
-                    <h1>Edit user info</h1>
-                    <form>
-                        <div className="input-wrapper">
-                            <label htmlFor="username">User name</label>
-                            <input
-                                onChange={(e) =>
-                                    setUsernameValue(e.target.value)
-                                }
-                                type="text"
-                                id="username"
-                                defaultValue={user.userInfo.userName}
-                            />
-                        </div>
-                        <div className="input-wrapper not-allowed">
-                            <label htmlFor="firstname">First name</label>
-                            <input
-                                type="text"
-                                id="firstname"
-                                defaultValue={user.userInfo.firstName}
-                                readOnly
-                                disabled
-                            />
-                        </div>
-                        <div className="input-wrapper not-allowed">
-                            <label htmlFor="lastname">Last name</label>
-                            <input
-                                type="text"
-                                id="lastname"
-                                defaultValue={user.userInfo.lastName}
-                                readOnly
-                                disabled
-                            />
-                        </div>
-                        <button
-                            className="save-button"
-                            disabled={usernameValue === ""}
-                            onClick={(e) => {
-                                updateUsername(e);
-                            }}
-                        >
-                            Save
-                        </button>
-                        <button
-                            onClick={() => setIsOpened(false)}
-                            className="cancel-button"
-                        >
-                            Cancel
-                        </button>
-                        {formError ? (
-                            <p className="error-edit-msg">{formError}</p>
-                        ) : null}
-                    </form>
-                </section>
+                <EditUsername isOpened={setIsOpened} />
             ) : (
                 <div className="header">
                     <h1>
